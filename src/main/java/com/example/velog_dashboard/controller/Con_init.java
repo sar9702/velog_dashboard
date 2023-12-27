d0b6184386cf8e4fe5bebf86722e44dcbe0544d7
aerim.shim <aerim.shim@rd101.co.kr>
2023-12-27 16:58:34 +0900

    메인 페이지에 Velog 게시물들이 보여지도록 함

--- src/main/java/com/example/velog_dashboard/controller/Con_init.java
+++ src/main/java/com/example/velog_dashboard/controller/Con_init.java
@@ -1,16 +1,27 @@
 package com.example.velog_dashboard.controller;
 
+import com.example.velog_dashboard.entity.Post;
+import com.example.velog_dashboard.service.PostService;
+import org.json.simple.parser.ParseException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/")
 public class Con_init {
 
+    @Autowired
+    PostService postService;
+
     @GetMapping("")
-    public String loadInitPage(Model model) {
+    public String loadInitPage(Model model) throws ParseException {
+        List<Post> posts = postService.posts();
+        model.addAttribute("posts", posts);
         return "/main/main";
     }
 }
