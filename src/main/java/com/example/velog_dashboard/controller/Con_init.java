dbc25183522fc2e57277678ed7d531afea94e792
aerim.shim <aerim.shim@rd101.co.kr>
2023-12-27 18:49:51 +0900

    게시물의 조회수가 보여지도록 함

--- src/main/java/com/example/velog_dashboard/controller/Con_init.java
+++ src/main/java/com/example/velog_dashboard/controller/Con_init.java
@@ -21,6 +21,13 @@ public class Con_init {
     @GetMapping("")
     public String loadInitPage(Model model) throws ParseException {
         List<Post> posts = postService.posts();
+
+        // 게시물마다 조회수를 가져온다.
+        for (int i = 0; i < posts.size(); i++) {
+            Post tmp = postService.stats(posts.get(i));
+            posts.set(i, tmp);
+        }
+
         model.addAttribute("posts", posts);
         return "/main/main";
     }
