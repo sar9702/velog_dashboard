1548475c3808015e780518dc310fbd69f329e4fa
aerim.shim <aerim.shim@rd101.co.kr>
2023-12-27 15:23:02 +0900

    init

---
+++ src/main/java/com/example/velog_dashboard/VelogDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.velog_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VelogDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VelogDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/velog_dashboard/VelogDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.velog_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VelogDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
